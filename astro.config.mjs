14dadf0979db3e0b002e4b2b69e494a193c7e362
Guillermo Villar Pinto <guillermo1858@gmail.com>
2025-04-04 13:44:57 -0300

    Tercera vers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://7ouzz-coder.github.io',
   base: '/portafolio-guillermo',
   integrations: [tailwind()],
-  // Configuración para generación de sitio estático
   output: 'static',
 });
--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+interface ImportMetaEnv {
+    // Agrega aquí variables de entorno si las necesitas
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
