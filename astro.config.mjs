8c94df4c153ea9b46f6e02a3240d10cebe061628
Guillermo Villar Pinto <guillermo1858@gmail.com>
2025-04-04 13:49:27 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// En astro.config.mjs
 export default defineConfig({
-  site: 'https://7ouzz-coder.github.io',
-  base: '/portafolio-guillermo',
+    site: 'https://7ouzz-coder.github.io',
+    base: '/7ouzz-coder.github.io',
+    outDir: './docs', // Cambia el directorio de salida a docs
+    output: 'static',
   integrations: [tailwind()],
-  output: 'static',
 });
