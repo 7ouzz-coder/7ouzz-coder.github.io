41a79963ab7f113c8f89a303a39fb448798e4bee
Guillermo Villar Pinto <guillermo1858@gmail.com>
2025-04-04 14:22:20 -0300

    Arreglando astroconfigmjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // En astro.config.mjs
 export default defineConfig({
     site: 'https://7ouzz-coder.github.io',
-    base: '/7ouzz-coder.github.io',
+    base: '',
     outDir: './dist', // Cambia el directorio de salida a docs
     output: 'static',
   integrations: [tailwind()],
